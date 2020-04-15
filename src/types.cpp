bee94eea305e9d94bd75c7d1218a5c153a0040a7
akowalew <ram.techen@gmail.com>
2020-04-15 22:08:03 +0200

    types: Add registers specifiers

--- src/types.cpp
+++ src/types.cpp
@@ -2,6 +2,7 @@
 // Definitions of most used type definitions
 ///////////////////////////////////////////////////////////////////////////////
 
+// Shorthands for sized-types
 using u8 = uint8_t;
 using i8 = int8_t;
 using u16 = uint16_t;
@@ -10,3 +11,9 @@ using u32 = uint32_t;
 using i32 = int32_t;
 using u64 = uint64_t;
 using i64 = int64_t;
+
+// Helper definitions used when declaring hardware blocks
+#define RW 
+#define RO const
+#define W1C
+#define RW1C
