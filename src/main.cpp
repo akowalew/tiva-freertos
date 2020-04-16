d86eab0af797a20da2b36b06486446bbbeeb44e0
akowalew <ram.techen@gmail.com>
2020-04-16 15:31:24 +0200

    main: Remove queue, tx and rx tasks. Leave writer, which puts to UART

--- src/main.cpp
+++ src/main.cpp
@@ -14,62 +14,60 @@
 
 #include "hw_sysctl.h"
 #include "hw_gpio.h"
+#include "hw_uart.h"
 
 #include "utils.cpp"
-#include "gpio.cpp"
 #include "sysctl.cpp"
+#include "gpio.cpp"
+#include "uart.cpp"
+#include "nvic.cpp"
 
 #include "handlers.cpp"
 
-// Queue to be used between reader and writer tasks
-static QueueHandle_t queue = NULL;
+// //
+// // Reader task
+// //
 
-//
-// Reader task
-//
+// static void rx_task([[maybe_unused]] void *params)
+// {
+// 	while(true)
+// 	{
+// 		// Get message from writer
+// 		u32 recv_value;
+// 		CHECK(xQueueReceive(queue, &recv_value, portMAX_DELAY));
+// 		CHECK(recv_value == 100UL);
 
-static void rx_task([[maybe_unused]] void *params)
-{
-	while(true)
-	{
-		// Get message from writer
-		u32 recv_value;
-		CHECK(xQueueReceive(queue, &recv_value, portMAX_DELAY));
-		CHECK(recv_value == 100UL);
+// 		// Flash the receive LED for a while
+// 		GPIOF->DATA[GREEN_LED_PIN] ^= GREEN_LED_PIN;
+// 		vTaskDelay(pdMS_TO_TICKS(100));
+// 		GPIOF->DATA[GREEN_LED_PIN] ^= GREEN_LED_PIN;
+// 		vTaskDelay(pdMS_TO_TICKS(100));
+// 	}
 
-		// Flash the receive LED for a while
-		GPIOF->DATA[GREEN_LED_PIN] ^= GREEN_LED_PIN;
-		vTaskDelay(pdMS_TO_TICKS(100));
-		GPIOF->DATA[GREEN_LED_PIN] ^= GREEN_LED_PIN;
-		vTaskDelay(pdMS_TO_TICKS(100));
-	}
-
-	while(1);
-}
+// 	while(1);
+// }
 
 //
 // Writer task
 //
 
-static void tx_task([[maybe_unused]] void *params)
+static void writer_task([[maybe_unused]] void *params)
 {
 	auto nextwaketime = xTaskGetTickCount();
 
 	while(true)
 	{
-		// Keep fixed update rate
-		vTaskDelayUntil(&nextwaketime, pdMS_TO_TICKS(1000));
-
 		// Flash the transmit LED for a while
 		GPIOF->DATA[RED_LED_PIN] ^= RED_LED_PIN;
 		vTaskDelay(pdMS_TO_TICKS(100));
 		GPIOF->DATA[RED_LED_PIN] ^= RED_LED_PIN;
 		vTaskDelay(pdMS_TO_TICKS(100));
 
-		// Put message to the reader
-		const auto value = 100UL;
-		const auto tickstowait = 0U;
-		CHECK(xQueueSend(queue, &value, tickstowait));
+		// Send some string through UART
+		uart_write("En taro Adun, Executor!\n");
+		
+		// Keep fixed update rate
+		vTaskDelayUntil(&nextwaketime, pdMS_TO_TICKS(1000));
 	} 
 }
 
@@ -79,25 +77,26 @@ static void tx_task([[maybe_unused]] void *params)
 
 int main()
 {
+	// Disable write cache, IT folding and interrupts on multicycle instructions
+	*(volatile u32*)(0xE000E008) |= 0x7;
+
 	// Hardware initialization
 	sys_init();
 	gpio_init();
+	uart_init();
+	nvic_init();
 
-	// Create queue to be used by tasks
-	const auto QUEUE_LENGTH = 1;
-	CHECK(queue = xQueueCreate(QUEUE_LENGTH, sizeof(u32)));
+	// Writer task
+	const auto writer_stacksize = configMINIMAL_STACK_SIZE;
+	const auto writer_params = nullptr;
+	const auto writer_priority = (tskIDLE_PRIORITY + 1);
+	CHECK(xTaskCreate(writer_task, "writer", writer_stacksize, writer_params, writer_priority, nullptr));
 
-	// Send task
-	const auto tx_stacksize = configMINIMAL_STACK_SIZE;
-	const auto tx_params = nullptr;
-	const auto tx_priority = (tskIDLE_PRIORITY + 1);
-	CHECK(xTaskCreate(tx_task, "tx", tx_stacksize, tx_params, tx_priority, nullptr));
-
-	// Receive task
-	const auto rx_stacksize = configMINIMAL_STACK_SIZE;
-	const auto rx_params = nullptr;
-	const auto rx_priority = (tskIDLE_PRIORITY + 2);
-	CHECK(xTaskCreate(rx_task, "rx", rx_stacksize, rx_params, rx_priority, nullptr));
+	// // Receive task
+	// const auto rx_stacksize = configMINIMAL_STACK_SIZE;
+	// const auto rx_params = nullptr;
+	// const auto rx_priority = (tskIDLE_PRIORITY + 2);
+	// CHECK(xTaskCreate(rx_task, "rx", rx_stacksize, rx_params, rx_priority, nullptr));
 
 	// Run kernel
 	vTaskStartScheduler();
