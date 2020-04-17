fef9dc3c7f22f0402f703b1ef9e197a9129b6191
akowalew <ram.techen@gmail.com>
2020-04-17 16:42:23 +0200

    FreeRTOSConfig: Reduce minimal stack size
    Most of the tasks doesn't need such a big stack

--- config/FreeRTOSConfig.h
+++ config/FreeRTOSConfig.h
@@ -7,7 +7,7 @@
 #define configCPU_CLOCK_HZ                      16000000
 #define configTICK_RATE_HZ                      250
 #define configMAX_PRIORITIES                    5
-#define configMINIMAL_STACK_SIZE                256
+#define configMINIMAL_STACK_SIZE                128
 #define configMAX_TASK_NAME_LEN                 16
 #define configUSE_16_BIT_TICKS                  0
 #define configIDLE_SHOULD_YIELD                 1
