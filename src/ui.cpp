75106d7f934afc69ee5733559cdf3ec252166505
akowalew <ram.techen@gmail.com>
2020-04-20 01:17:56 +0200

    ui: Refactor against writing to display RAM instead of writing to buffer

--- src/ui.cpp
+++ src/ui.cpp
@@ -6,34 +6,46 @@
 // Private functions
 //
 
-const auto BIG_DIGIT_HEIGHT = 4;
+const auto BIG_DIGIT_PAGES = 4;
 const auto BIG_DIGIT_WIDTH = 16;
-const auto SMALL_DIGIT_HEIGHT = 2;
+const auto SMALL_DIGIT_PAGES = 2;
 const auto SMALL_DIGIT_WIDTH = 8;
 
-void draw_big_digit(u8 digit, u8 x, u8 y)
+void draw_big_digit(u8 digit, u8 x, u8 page)
 {
-	for(u8 j = 0; j < BIG_DIGIT_HEIGHT; ++j) 
-		memcpy(&ssd1306_data[y+j][x], &digits_big[digit][j][0], BIG_DIGIT_WIDTH);
+	for(u8 j = 0; j < BIG_DIGIT_PAGES; ++j) 
+	{
+		ssd1306_setpos(x, page+j);
+		ssd1306_write_data(digits_big[digit][j]);
+	}
 }
 
-void draw_small_digit(u8 digit, u8 x, u8 y) {
-	for(u8 j = 0; j < SMALL_DIGIT_HEIGHT; ++j) 
-		memcpy(&ssd1306_data[y+j][x], &digits_small[digit][j][0], SMALL_DIGIT_WIDTH);
+void draw_small_digit(u8 digit, u8 x, u8 page)
+{
+	for(u8 j = 0; j < SMALL_DIGIT_PAGES; ++j) 
+	{
+		ssd1306_setpos(x, page+j);
+		ssd1306_write_data(digits_small[digit][j]);
+	}
 }
 
-void draw_colon(u8 x, u8 y) {
-	for(u8 j = 0; j < BIG_DIGIT_HEIGHT; ++j) 
-		memcpy(&ssd1306_data[y+j][x], &colon[j][0], BIG_DIGIT_WIDTH);
+void draw_colon(u8 x, u8 page) 
+{
+	for(u8 j = 0; j < BIG_DIGIT_PAGES; ++j) 
+	{
+		ssd1306_setpos(x, page+j);
+		ssd1306_write_data(colon[j]);
+	}
 }
 
 static void ui_task([[maybe_unused]] void* params)
 {
+	// Perform display's chip initialization and turn it ON
 	ssd1306_startup();
 	
 	// Static clock's digits display configuration
-	constexpr auto big_y = 2;
-	constexpr auto small_y = 4;
+	constexpr auto big_page = 2;
+	constexpr auto small_page = 4;
 	constexpr auto spacing_big = 4;
 	constexpr auto spacing_small = 2;
 	constexpr auto hour_hi_x = 8;
@@ -45,7 +57,7 @@ static void ui_task([[maybe_unused]] void* params)
 	constexpr auto second_lo_x = second_hi_x+SMALL_DIGIT_WIDTH+spacing_small;
 
 	// Colon will not change, so we can draw it to the buffer only once
-	draw_colon(colon_x, big_y);
+	draw_colon(colon_x, big_page);
 
 	while(true)
 	{
@@ -74,13 +86,13 @@ static void ui_task([[maybe_unused]] void* params)
 			if(hour != prev_hour) {
 				const auto hour_hi = (hour / 10);
 				if(hour_hi != prev_hour_hi) {
-					draw_big_digit(hour_hi, hour_hi_x, big_y);
+					draw_big_digit(hour_hi, hour_hi_x, big_page);
 					prev_hour_hi = hour_hi;
 				}
 
 				const auto hour_lo = (hour % 10);
 				if(hour_lo != prev_hour_lo) {
-					draw_big_digit(hour_lo, hour_lo_x, big_y);
+					draw_big_digit(hour_lo, hour_lo_x, big_page);
 					prev_hour_lo = hour_lo;
 				}
 
@@ -91,13 +103,13 @@ static void ui_task([[maybe_unused]] void* params)
 			if(minute != prev_minute) {
 				const auto minute_hi = (minute / 10);
 				if(minute_hi != prev_minute_hi) {
-					draw_big_digit(minute_hi, minute_hi_x, big_y);
+					draw_big_digit(minute_hi, minute_hi_x, big_page);
 					prev_minute_hi = minute_hi;
 				}
 
 				const auto minute_lo = (minute % 10);
 				if(minute_lo != prev_minute_lo) {
-					draw_big_digit(minute_lo, minute_lo_x, big_y);
+					draw_big_digit(minute_lo, minute_lo_x, big_page);
 					prev_minute_lo = minute_lo;
 				}
 
@@ -107,31 +119,28 @@ static void ui_task([[maybe_unused]] void* params)
 			// Higher digit of seconds may change, but not often
 			const auto second_hi = (second / 10);
 			if(second_hi != prev_second_hi) {
-				draw_small_digit(second_hi, second_hi_x, small_y);
+				draw_small_digit(second_hi, second_hi_x, small_page);
 				prev_second_hi = second_hi;
 			}
 
 			// Lower digit of seconds will for sure change...
 			const auto second_lo = (second % 10);
-			draw_small_digit(second_lo, second_lo_x, small_y);
-
-			// Write changes to the display
-			ssd1306_write();
+			draw_small_digit(second_lo, second_lo_x, small_page);
 
 			// Wait for the next second
 			// NOTE: It can be done with RTC actually, but it brings some complications...
 			vTaskDelayUntil(&nextwaketime, pdMS_TO_TICKS(1000));
 		}
 
-		// Let the display be black
-		// TODO: The best will be simply to put display to sleep 
-		//       and then wake it up.
-		ssd1306_clear();
-		ssd1306_write();
+		// Turn off the display. It will be turned on after user action
+		ssd1306_display_off();
 
 		// Wait for any button to be pressed
 		const auto buttons = buttons_read();
 		assert(buttons != 0x00);
+
+		// Let the display be working again
+		ssd1306_display_on();
 	}
 }
 
