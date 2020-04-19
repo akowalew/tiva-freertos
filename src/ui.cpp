45703020904f10bff071cdc80bf2a56d265a660d
akowalew <ram.techen@gmail.com>
2020-04-19 23:35:53 +0200

    ui: Show time only after button hit, else display nothing (turn it off)

--- src/ui.cpp
+++ src/ui.cpp
@@ -10,60 +10,75 @@ static void ui_task([[maybe_unused]] void* params)
 {
 	ssd1306_startup();
 	
-	bool show_colon = false;
 	while(true)
 	{
+		// Let the display be black
+		// TODO: The best will be simply to put display to sleep 
+		//       and then wake it up.
 		ssd1306_clear();
-		const auto time = hib_rtc_seconds();
+		ssd1306_write();
 
-		const u8 hour = ((time / 3600) % 24);
-		const u8 minute = ((time / 60) % 60);
-		const u8 second = (time % 60);
+		// Wait for any button to be pressed
+		const auto buttons = buttons_read();
+		assert(buttons != 0x00);
 
-		const u8 hour_hi = (hour / 10);
-		const u8 hour_lo = (hour % 10);
+		// For some duration display current time
+		auto nextwaketime = xTaskGetTickCount();
+		for(u8 i = 0; i < 5; ++i)
+		{
+			ssd1306_clear();
+			const auto time = hib_rtc_seconds();
 
-		const u8 minute_hi = (minute / 10);
-		const u8 minute_lo = (minute % 10);
+			const u8 hour = ((time / 3600) % 24);
+			const u8 minute = ((time / 60) % 60);
+			const u8 second = (time % 60);
 
-		const u8 second_hi = (second / 10);
-		const u8 second_lo = (second % 10);
+			const u8 hour_hi = (hour / 10);
+			const u8 hour_lo = (hour % 10);
 
-		u8 y = 2;
-		u8 x = 8;
-		u8 spacing_big = 4;
-		u8 spacing_small = 2;
+			const u8 minute_hi = (minute / 10);
+			const u8 minute_lo = (minute % 10);
 
-		for(u8 j = 0; j < 4; ++j) 
-			memcpy(&ssd1306_data[j+y][x], &digits_big[hour_hi][j][0], 16);
-		x += (16 + spacing_big);
+			const u8 second_hi = (second / 10);
+			const u8 second_lo = (second % 10);
 
-		for(u8 j = 0; j < 4; ++j) 
-			memcpy(&ssd1306_data[j+y][x], &digits_big[hour_lo][j][0], 16);
-		x += (16 + spacing_big);
+			u8 y = 2;
+			u8 x = 8;
+			u8 spacing_big = 4;
+			u8 spacing_small = 2;
 
-		if((show_colon = !show_colon))
+			for(u8 j = 0; j < 4; ++j) 
+				memcpy(&ssd1306_data[j+y][x], &digits_big[hour_hi][j][0], 16);
+			x += (16 + spacing_big);
+
+			for(u8 j = 0; j < 4; ++j) 
+				memcpy(&ssd1306_data[j+y][x], &digits_big[hour_lo][j][0], 16);
+			x += (16 + spacing_big);
+
 			for(u8 j = 0; j < 4; ++j) 
 				memcpy(&ssd1306_data[j+y][x], &colon[j][0], 16);
-		x += (16 + spacing_big);
+			x += (16 + spacing_big);
 
-		for(u8 j = 0; j < 4; ++j) 
-			memcpy(&ssd1306_data[j+y][x], &digits_big[minute_hi][j][0], 16);
-		x += (16 + spacing_big);
+			for(u8 j = 0; j < 4; ++j) 
+				memcpy(&ssd1306_data[j+y][x], &digits_big[minute_hi][j][0], 16);
+			x += (16 + spacing_big);
 
-		for(u8 j = 0; j < 4; ++j) 
-			memcpy(&ssd1306_data[j+y][x], &digits_big[minute_lo][j][0], 16);
-		x += (16 + spacing_big);
+			for(u8 j = 0; j < 4; ++j) 
+				memcpy(&ssd1306_data[j+y][x], &digits_big[minute_lo][j][0], 16);
+			x += (16 + spacing_big);
 
-		for(u8 j = 0; j < 2; ++j) 
-			memcpy(&ssd1306_data[j+y+2][x], &digits_small[second_hi][j][0], 8);
-		x += (8 + spacing_small);
+			for(u8 j = 0; j < 2; ++j) 
+				memcpy(&ssd1306_data[j+y+2][x], &digits_small[second_hi][j][0], 8);
+			x += (8 + spacing_small);
 
-		for(u8 j = 0; j < 2; ++j) 
-			memcpy(&ssd1306_data[j+y+2][x], &digits_small[second_lo][j][0], 8);
-		x += (8 + spacing_small);
+			for(u8 j = 0; j < 2; ++j) 
+				memcpy(&ssd1306_data[j+y+2][x], &digits_small[second_lo][j][0], 8);
+			x += (8 + spacing_small);
 
-		ssd1306_write();
+			ssd1306_write();
+
+			vTaskDelayUntil(&nextwaketime, pdMS_TO_TICKS(1000));
+		}
 	}
 }
 
